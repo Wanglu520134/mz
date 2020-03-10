aadbaf857e214f27dc50d94b1f93d5948172d479
王露 <376079947@qq.com>
2020-03-10 21:40:53 +0800

    提交

--- src/views/Center.vue
+++ src/views/Center.vue
@@ -5,10 +5,7 @@
 </template>
 <script>
 export default {
-<<<<<<< HEAD
   // 路由的钩子函数
-=======
->>>>>>> 533a9d0b191cbfb46a84cffc7d88cb9d5563d3a8
   beforeRouteEnter (to, from, next) {
     // 局部路由守卫
     console.log('拦截')
